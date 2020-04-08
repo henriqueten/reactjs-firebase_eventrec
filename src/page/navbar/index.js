5144e942a97a2defd3eaf27585e88be6e989f87d
jakes <fjakis@gmail.com>
2020-04-07 23:21:45 -0300

    ajuste do navbar, firebase e .env

--- src/page/navbar/index.js
+++ src/page/navbar/index.js
@@ -11,14 +11,25 @@ function Navbar() {
 
     return(
 <nav class="navbar navbar-expand-lg" >
-        <i class="far fa-calendar-plus text-white fa-2x"></i>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars text-white"></i>
+        <i class="far fa-calendar-plus text-white fa-2x"/>
+        <button className="navbar-toggler" 
+        type="button" 
+        data-toggle="collapse" 
+        data-target="#navbarSupportedContent" 
+        aria-controls="navbarSupportedContent" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
+        <i class="fas fa-bars text-white"/>
         </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
-      <li className="nav-item"><Link className="nav-link ml-2" to="/">Home </Link></li>
+      <li className="nav-item">
+        <Link className="nav-link ml-2" to="/">Home </Link>
+      </li>
+      <li className="nav-item"><Link className="nav-link" to="/usuario/novo">Cadastrar </Link></li>
+
+      
 
       {
       useSelector( state => state.usuarioLogado) > 0 ?
@@ -29,7 +40,6 @@ function Navbar() {
       </>
       :
       <>
-      <li className="nav-item"><Link className="nav-link" to="/usuario/novo">Cadastrar </Link></li>
       <li className="nav-item"><Link className="nav-link" to="/login">Login </Link></li>
       </>
 
